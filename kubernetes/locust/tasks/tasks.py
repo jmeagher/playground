dba7ab118b9252d38559e476e12238bf52522733
John Meagher <john.meagher@gmail.com>
2017-10-09 18:54:59 -0700

    Improve the monitoring cluster setup and log performance stats from the locust test

--- kubernetes/locust/tasks/tasks.py
+++ kubernetes/locust/tasks/tasks.py
@@ -2,11 +2,13 @@ import datetime
 import glob
 import os
 import random
+import requests
+import sys
 import time
 
 from locust import HttpLocust, TaskSet, task
 
-timeout = (2, 20)
+timeout = (20, 200)
 corpus_glob = os.environ["CORPUS_GLOB"]
 all_files = glob.glob(corpus_glob)
 
@@ -18,6 +20,27 @@ simple_queries = [
     'random:50 AND data_type:medium_cardinality_insert'
 ]
 
+monitoring_es = os.environ.get("MONITORING_ES_URL", None)
+print "Monitoring_es = '%s'" % monitoring_es
+
+def monitored(func):
+    def wrapper(*arg, **kw):
+        timestamp = datetime.datetime.utcnow().isoformat()
+        start = time.time()
+        out = func(*arg, **kw)
+        end = time.time()
+        print "Something %s" % monitoring_es
+        if monitoring_es:
+            print "Sending stats"
+            requests.request('POST', monitoring_es, json={
+              "timestamp": timestamp,
+              "response_time": (end-start),
+              "test": func.__name__,
+              "success": out[0],
+            })
+        return out
+    return wrapper
+
 class MixedElasticSearchLoad(TaskSet):
 
     def on_start(self):
@@ -27,52 +50,74 @@ class MixedElasticSearchLoad(TaskSet):
         self.corpus = [ l.strip() for l in raw if len(l) > 2]
 
     @task(100)
+    @monitored
     def low_cardinality_insert(self):
-        self._do_insert(random.randint(0,100), "low_cardinality_insert")
+        return self._do_insert(random.randint(0,100), "low_cardinality_insert")
 
     @task(100)
+    @monitored
     def medium_cardinality_insert(self):
-        self._do_insert(random.randint(0,10000), "medium_cardinality_insert")
+        return self._do_insert(random.randint(0,10000), "medium_cardinality_insert")
 
     @task(1000)
+    @monitored
     def high_cardinality_insert(self):
-        self._do_insert(random.randint(0,10000000), "high_cardinality_insert")
+        return self._do_insert(random.randint(0,10000000), "high_cardinality_insert")
 
     @task(100)
+    @monitored
     def simple_search(self):
-        self._do_search("simple_search", simple_search=random.choice(simple_queries))
+        return self._do_search("simple_search", simple_search=random.choice(simple_queries))
 
     def _do_search(self, name, simple_search=None, post_search=None):
         if simple_search:
             url = "/%s/_search?q=%s" % (test_index, simple_search)
         else:
             url = "/%s/_search" % test_index
-        response = self.client.get(url, name=name,
-            json=post_search, timeout=timeout)
-        self._do_validation(response)
+        with self.client.get(url, name=name,
+            json=post_search, timeout=timeout, catch_response=True) as response:
+          return self._do_validation(response)
 
     def _do_insert(self, id, name):
         line = random.choice(self.corpus)
         data = {
-            "time1": int(time.time()),
-            "time2": 1000*int(time.time()),
             "timestamp": datetime.datetime.utcnow().isoformat(),
             "line": line,
             "my_id": id,
             "random": random.randint(0,1000),
             "data_type": name,
         }
-        response = self.client.put(
+        with self.client.put(
             "/%s/test/%s-%s" % (test_index, name, id),
-            json=data, name=name, timeout=timeout)
-        self._do_validation(response)
+            json=data, name=name, timeout=timeout, catch_response=True) as response:
+          return self._do_validation(response, expected_response_codes=[200,201])
 
-    def _do_validation(self, response):
-        assert response.status_code == 200
-        out = response.json()
-        shards = out["_shards"]
-        assert shards["successful"] > 0
-        assert shards["failed"] == 0
+    def _do_validation(self, response, expected_response_codes=[200]):
+        successful_request = False
+        try:
+            try:
+              out = response.json()
+            except (ValueError) as e:
+                response.failure("Didn't get json as a response, status: %s  text: %s" % (response.status_code, response.text))
+            if not response.status_code in expected_response_codes:
+                response.failure("Expected response code %s but got %s instead" %
+                  (expected_response_codes, response.status_code) )
+            elif not "_shards" in out:
+                response.failure("No _shards information is available")
+            else:
+              shards = out["_shards"]
+              successful = shards.get("successful", -1)
+              failed = shards.get("failed", 999)
+              if successful <= 0:
+                  response.failure("Expected successful shards, but got %s instead" % str(successful) )
+              elif failed > 0:
+                  response.failure("Expected no failed shards, but got %s instead" % str(failed) )
+              else:
+                  successful_request = True
+                  response.success()
+        except:
+            response.failure("Unknown exception: " + str("\n".join(sys.exc_info())))
+        return (successful_request, None)
 
 
 class ElasticSearchUser(HttpLocust):
